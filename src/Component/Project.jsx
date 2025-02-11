83abec0d2eb242202f1e49ef91a2cb0122075d63
haile eshetu <michaelhaile1222@gmail.com>
2025-02-11 22:25:44 +0300

    the correction in the project

--- src/Component/Project.jsx
+++ src/Component/Project.jsx
@@ -3,38 +3,53 @@ import "./Project.css";
 import project1 from "../assets/azmeraTrade.jpg";
 import project2 from "../assets/netflix.jpg";
 import project3 from "../assets/smartmenu.jpg";
+import project4 from "../assets/studynest.jpg";
 
 const Project = () => {
   const projects = [
     {
       id: 1,
       image: project1,
+      title: "Azmera Trade",
       github: "https://github.com/user/project1",
     },
     {
       id: 2,
       image: project2,
-      github: "https://github.com/user/project2",
+      title: "Netflix Clone",
+      github: "https://github.com/hailegithubaccount/netflix-by-react.git",
     },
     {
       id: 3,
       image: project3,
+      title: "Smart Menu",
       github: "https://github.com/user/project3",
+    },
+    {
+      id: 4,
+      image: project4,
+      title: "Study Nest",
+      github: "https://github.com/user/project4",
     },
   ];
 
   return (
-    <div className="projects-grid">
-      {projects.map((project) => (
-        <div key={project.id} className="project-card">
-          <img src={project.image} alt={`Project ${project.id}`} />
-          <div className="overlay">
-            <a href={project.github} target="_blank" rel="noopener noreferrer">
-              View on GitHub
-            </a>
+    <div>
+      <div className="text">My Projects</div>
+
+      <div className="projects-grid">
+        {projects.map((project) => (
+          <div key={project.id} className="project-card">
+            <img src={project.image} alt={`Project ${project.id}`} />
+            <h3 className="project-title">{project.title}</h3>
+            <div className="overlay">
+              <a href={project.github} target="_blank" rel="noopener noreferrer">
+                View on GitHub
+              </a>
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   );
 };
